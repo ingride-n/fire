26e32eade866958ac49811ca28ab7f25e6269752
Ingride Ngaku <ingride.i.ngaku@gmail.com>
2021-09-28 01:22:56 -0400

    Create and style home page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
-import './styles.css';
+import { Box, Paper } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { useState } from "react";
+import "./styles.css";
 import ReverseSalaryCalculator from "./ReverseSalaryCalculator";
 
+const useStyles = makeStyles(() => ({
+  item: {
+    width: "150px",
+    height: "150px",
+    padding: "20px",
+    fontSize: "25px",
+    textAlign: "center",
+    fontWeight: "bold",
+    // margin: "auto",
+    backgroundColor: "lavender",
+  },
+  box: {
+    display: "flex",
+    // margin: "auto",
+    // justifyContent: "space-evenly",
+    // border: "1px solid black"
+  },
+}));
+
 function App() {
+  const classes = useStyles();
+  const [show, setShow] = useState(null);
+  const products = ["Reverse Salary Calculator"];
   return (
     <div className="App">
-      <ReverseSalaryCalculator />
-      <div></div>
+      <div id="site-header">
+        <h1>AIVIE</h1>
+        <p>Financial Independence Toolkit</p>
+      </div>
+      {show === 0 ? <ReverseSalaryCalculator /> : null}
+      {!show ? (
+        <Box className={classes.box}>
+          {products.map((product) => (
+            <Paper className={classes.item}>{product}</Paper>
+          ))}
+        </Box>
+      ) : null}
     </div>
   );
 }
